fe4bf39d168449aa6995e969bd0f1d9e07ad48c7
yurkohudyma <hudyma@gmail.com>
2025-11-10 14:08:04 +0200

    init commit

---
+++ src/main/java/ua/hudyma/LinkedIn2025App.java
@@ -0,0 +1,11 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedIn2025App {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedIn2025App.class, args);
+    }
+}
